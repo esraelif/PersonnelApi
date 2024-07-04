f2c5bfe09d030915c3b46a2af6f1c124c7c98e03
esraelif <elifesratunca@gmail.com>
2024-07-04 10:50:03 +0200

    "Commented out require('./src/helpers/sync')() and updated a comment"

--- index.js
+++ index.js
@@ -78,5 +78,5 @@ app.use(require('./src/middlewares/errorHandler'))
 // RUN SERVER:
 app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
 
-/* ------------------------------------------------------- */
+/* ---------------------Fake veri oluşturması ıcın(1 kere calıstırdık-KAPATTIK)---------------------------------- */
 // require("./src/helpers/sync")()
